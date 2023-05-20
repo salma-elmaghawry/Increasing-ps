0624b53a2e90b4ef4707c7954035cd7a3df440a8
salma Elmaghawry <112850475+salma-elmaghawry@users.noreply.github.com>
2023-05-20 19:28:40 +0300

    Add files via upload

---
+++ Project15/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+        bool f = 0;
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] == arr[i + 1])
+            {
+                {
+                    f = 1;
+                }
+            }
+        if (f)cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+}
